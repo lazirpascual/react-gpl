73d17dc1832aeed814f101125a26e0a7410be94f
Lazir Pascual <lazirpascual@gmail.com>
2022-05-03 01:18:50 -0400

    create custom hook to fetch an individual character

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import "./App.css";
+import { Route, Routes } from "react-router";
 import CharactersList from "./pages/CharactersList";
+import Character from "./pages/Character";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <CharactersList />
+      <Routes>
+        <Route strict exact path="/" element={<CharactersList />} />
+        <Route strict exact path="/:id" element={<Character />} />
+      </Routes>
     </div>
   );
 }
